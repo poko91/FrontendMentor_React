dd2153d3b139ec01bf7f2d8168fc346da2ac6d71
Poorna Kamat <kamatpoorna@gmail.com>
2024-05-03 15:43:07 +0530

    fix: added link to back-button in Error page, and modified css

--- Rest-countries-api/components/Error.jsx
+++ Rest-countries-api/components/Error.jsx
@@ -1,13 +1,17 @@
+import { Link } from "react-router-dom";
+
 export default function Error() {
   return (
     <main className="error-page-container">
       <h2>404</h2>
       <p>Page not found</p>
       <p>Oops!, the page you looking for does not exist</p>
-      <button className="back-button">
-        <i className="fa-solid fa-arrow-left-long"></i>
-        Back to home
-      </button>
+      <Link to="/">
+        <button className="error-back-button">
+          <i className="fa-solid fa-arrow-left-long"></i>
+          Back to home
+        </button>
+      </Link>
     </main>
   );
 }
