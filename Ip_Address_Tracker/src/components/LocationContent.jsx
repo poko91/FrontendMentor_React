289e4d3386108dc3898535715c026cfa2ee1f211
Poorna Kamat <kamatpoorna@gmail.com>
2024-05-07 15:21:58 +0530

    fix: changed ipData varibles

--- Ip_Address_Tracker/src/components/LocationContent.jsx
+++ Ip_Address_Tracker/src/components/LocationContent.jsx
@@ -1,23 +1,23 @@
 import React from "react";
 
-export default function LocationContent({ location }) {
+export default function LocationContent({ ipdata }) {
   return (
     <div className="location-container">
       <div className="loc-item">
         <p className="loc-title">IP ADDRESS</p>
-        <p className="loc-content">192.212.174.101</p>
+        <p className="loc-content">{ipData.ip}</p>
       </div>
       <div className="loc-item">
         <p className="loc-title">LOCATION</p>
-        <p className="loc-content">{`${location.city}, ${location.country} ${location.postalCode}`}</p>
+        <p className="loc-content">{`${ipData.location.city}, ${ipData.location.country} ${ipData.location.postalCode}`}</p>
       </div>
       <div className="loc-item">
         <p className="loc-title">TIMEZONE</p>
-        <p className="loc-content">{location.timezone}</p>
+        <p className="loc-content">{ipData.timezone}</p>
       </div>
       <div className="loc-item">
         <p className="loc-title">ISP</p>
-        <p className="loc-content">{location.isp}</p>
+        <p className="loc-content">{ipData.isp}</p>
       </div>
     </div>
   );
