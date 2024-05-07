31be3cd0478da8ec408fabbaae6e4fbac9696dd2
Poorna Kamat <kamatpoorna@gmail.com>
2024-05-07 14:14:55 +0530

    fix: changed searchbar icon

--- Ip_Address_Tracker/src/components/SearchBar.jsx
+++ Ip_Address_Tracker/src/components/SearchBar.jsx
@@ -1,8 +1,12 @@
+import { FaAngleRight } from "react-icons/fa";
+
 export default function SearchBar() {
   return (
     <div className="search-bar-container">
       <input type="search" className="search-bar" placeholder="Search for any IP address or domain" />
-      <button>{`>`}</button>
+      <button>
+        <FaAngleRight />
+      </button>
     </div>
   );
 }
